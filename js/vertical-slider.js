c0bf5e0798d54ef42fbc26341678c53ab9e0885c
dmitbotvd <dmitbotvd@gmail.com>
2020-07-31 17:29:43 +0300

    need integrations slider portfolio

--- js/vertical-slider.js
+++ js/vertical-slider.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		menu: '#menu',
 		lockAnchors: false,
-		anchors:['1', '2', '3', '4'],
+		anchors:['1', '2', '3', '4', '5'],
 		navigation: true,
 		navigationPosition: 'right',
-		navigationTooltips: ['1', '2', '3', '4'],
+		navigationTooltips: ['1', '2', '3', '4', '5'],
 		showActiveTooltip: false,
 		slidesNavigation: false,
 		slidesNavPosition: 'bottom',
